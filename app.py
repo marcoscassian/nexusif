514a8790ce4792a94115793d99333afc14f07bc9
marcoscassian <161905696+marcoscassian@users.noreply.github.com>
2025-08-01 17:02:48 -0300

    Update app.py
    edicao dos cards

--- app.py
+++ app.py
@@ -7,28 +7,32 @@ def index():
     cards = [
         {   
             "id": 1,
-            "titulo": "Projeto 1",
-            "descricao": "Descrição do projeto 1",
+            "titulo": "IFNexus",
+            "descricao": "O IFNexus é uma vitrine digital desenvolvida para divulgar e valorizar os projetos criados por estudantes e servidores do IFRN.",
             "imagem": "/static/img1.jpg",
             "tag": "Informática",
-            "cor_tag": "blue"
         },
         {
             "id": 2,
-            "titulo": "Projeto 2",
-            "descricao": "Descriçãos do projeto 2",
+            "titulo": "SIMER",
+            "descricao": "Sistema que monitora o consumo de energia em tempo real, identifica os maiores gastos e sugere formas de economizar.",
             "imagem": "/static/img2.jpg",
-            "tag": "Eletro",
-            "cor_tag": "green"
+            "tag": "Eletrotécnica",
         },
         {
             "id": 3,
-            "titulo": "Projeto 3",
-            "descricao": "Descriçãos do projeto 3",
+            "titulo": "EcoFios",
+            "descricao": "Projeto voltado à produção de fios ecológicos reutilizando sobras de tecido. Busca reduzir o desperdício na indústria têxtil.",
             "imagem": "/static/img3.jpg",
-            "tag": "Informatica",
-            "cor_tag": "green"
-        }
+            "tag": "Têxtil",
+        },
+        {
+            "id": 4,
+            "titulo": "Modus",
+            "descricao": "Criação de roupas sustentáveis usando materiais ecológicos para reduzir o impacto ambiental da moda.",
+            "imagem": "/static/img4.jpg",
+            "tag": "Vestuário",
+        },
         
     ]
     return render_template('index.html', cards=cards)
